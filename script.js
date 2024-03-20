e1349a85d229b6b9b55b7994d6645411ffee6c6b
Larry <123266512+prayForPluto@users.noreply.github.com>
2024-03-20 15:55:57 -0700

    Updated script file by adding dummy books to array.

--- script.js
+++ script.js
@@ -13,8 +13,33 @@ function addBookToLibrary(Book) {
 
 function getBooks() {
     for (const book of myLibrary) {
-        alert(book);
+        alert(book.title);
     }
 }
 
-displayBooks();
+const bookOne = new Book(
+    "Michael Punke",
+    "The Revenant",
+    272,
+    false,
+);
+
+const bookTwo = new Book(
+    "Cormac McCarthy",
+    "Blood Meridian",
+    353,
+    false,
+);
+
+const bookThree = new Book(
+    "George Orwell",
+    "Animal Farm",
+    108,
+    true
+);
+
+addBookToLibrary(bookOne);
+addBookToLibrary(bookTwo);
+addBookToLibrary(bookThree);
+
+getBooks();
