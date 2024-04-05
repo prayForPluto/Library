843d8ba1c1d5ca4d165b71745dd556ccc1d95297
Larry <123266512+prayForPluto@users.noreply.github.com>
2024-04-05 16:03:44 -0700

    Added form button submit functionality

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const myLibrary = [];
 
-function Book(author, title, pages, read) {
+function Book(author, title, pages, read, bookID) {
     this.author = author;
     this.title = title;
     this.pages = pages;
     this.read = read;
+    this.bookId = bookID;
+
+    function getBookId() {
+        return this.bookId;
+    }
 }
 
 function addBookToLibrary() {
+    let bookID = 0;
+
     const author = prompt("Who is the author of the book?")
     const title = prompt("What is the title of the book?")
     const pages = prompt("How many pages does the book have?")
     const read = prompt("Has the book been read?")
-    myLibrary.push(new Book(author, title, pages, read))
+    myLibrary.push(new Book(author, title, pages, read, bookID))
+
+    
 }
 
 function getBooks(array) {
@@ -40,7 +49,7 @@ function getBooks(array) {
     }
 }
 
-const bookOne = new Book(
+/*const bookOne = new Book(
     "Michael Punke",
     "The Revenant",
     272,
@@ -59,7 +68,7 @@ const bookThree = new Book(
     "Animal Farm",
     108,
     "no",
-);
+); */
 
 const container = document.querySelector(".main-container")
 
@@ -80,6 +89,33 @@ bookButton.addEventListener("click", function bookForm() {
     form.setAttribute("name", "Author");
     form.setAttribute("placeholder", "Charles Dickens")
 
+    const title = document.createElement("input")
+    form.setAttribute("type", "text")
+    form.setAttribute("name", "Title");
+    form.setAttribute("placeholder", "Rye With Cheese")
+
+    const pages = 0;
+
+    const read = 0;
+
+    const remove = 0;
+
+    const submitButton = document.createElement("input");
+    submitButton.type = 'submit';
+    submitButton.value = 'Submit';
+
     form.appendChild(author)
+    form.appendChild(title);
+    form.appendChild(submitButton);
     container.appendChild(form);
+
+    submitButton.addEventListener("click", event => {
+        event.preventDefault();
+        closeMe();
+    })
 });
+
+/* bookOne.incrementID();
+console.log(bookOne);
+console.log(bookTwo);
+console.log(bookThree); */
